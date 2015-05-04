0aef6625c6c635a3874340853a3410b1cc5e79b8
Felix <Felix@10.31.5.30>
2015-05-04 11:28:13 +0200

    new commit

---
+++ src/minichess2009/MiniChess2009.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minichess2009;
+
+/**
+ *
+ * @author Felix
+ */
+public class MiniChess2009 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
